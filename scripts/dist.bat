0d5db47f3b9cd9a2f7875fcf18ba51a68b788eae
Sam Hughes <samhughes1701@hotmail.co.uk>
2023-01-13 05:11:11 +0000

    remove old scripts dir

--- scripts/dist.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-@REM npm run build
-
-rmdir /s /q .\dist\npm\exhibitor\lib\
-xcopy .\build\site\server\ .\dist\npm\exhibitor\lib\site\server\ /s /e
-xcopy .\build\site\client\ .\dist\npm\exhibitor\lib\site\client\ /s /e
-xcopy .\build\api\ .\dist\npm\exhibitor\lib\api\ /s /e
-xcopy .\build\cli\cli\ .\dist\npm\exhibitor\lib\cli\ /s /e
