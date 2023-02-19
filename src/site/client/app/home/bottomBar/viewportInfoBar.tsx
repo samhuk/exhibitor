917eea5e3e16e5f4e69709cefca54ac9ab81bc8c
samhuk <samhughes1701@hotmail.co.uk>
2023-02-19 19:23:38 +0000

    Implement viewport out-of-bounds indication

--- src/site/client/app/home/bottomBar/viewportInfoBar.tsx
+++ src/site/client/app/home/bottomBar/viewportInfoBar.tsx
@@ -1,21 +1,60 @@
-import React, { KeyboardEvent } from 'react'
+import React, { KeyboardEvent, useEffect, useRef, useState } from 'react'
 import { useAppDispatch, useAppSelector } from '../../../store'
 import { applyWorkingViewportSize, updateWorkingViewportSize } from '../../../store/componentExhibits/actions'
 
+const determineSizeRestrictions = (
+  el: Element,
+  desiredSize: { width: number, height: number },
+): { width: boolean, height: boolean } => {
+  const actualSize = el.getBoundingClientRect()
+  const isWidthUnder = actualSize.width < desiredSize.width
+  const isHeightUnder = actualSize.height < desiredSize.height
+
+  return {
+    width: isWidthUnder,
+    height: isHeightUnder,
+  }
+}
+
+const updateSizeRestrictions = (
+  currentSizeRestrictions: { width: boolean, height: boolean },
+  newSizeRestrictions: { width: boolean, height: boolean },
+  setIsWidthRestricted: (newValue: boolean) => any,
+  setIsHeightRestricted: (newValue: boolean) => any,
+) => {
+  if (newSizeRestrictions.width !== currentSizeRestrictions.width)
+    setIsWidthRestricted(newSizeRestrictions.width)
+  if (newSizeRestrictions.height !== currentSizeRestrictions.height)
+    setIsHeightRestricted(newSizeRestrictions.height)
+}
+
 export const render = () => {
   const dispatch = useAppDispatch()
+  const [isWidthRestricted, setIsWidthRestricted] = useState(false)
+  const [isHeightRestricted, setIsHeightRestricted] = useState(false)
+  const size = useAppSelector(s => s.componentExhibits.viewportRectSizePx)
   const workingSize = useAppSelector(s => s.componentExhibits.workingViewportRectSizePx)
+  const el = useRef(document.getElementsByClassName('iframe-container')[0])
+
+  updateSizeRestrictions(
+    { width: isWidthRestricted, height: isHeightRestricted },
+    determineSizeRestrictions(el.current, size),
+    setIsWidthRestricted,
+    setIsHeightRestricted,
+  )
+
+  const _applyWorkingViewportSize = () => {
+    dispatch(applyWorkingViewportSize())
+  }
 
   const onKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
     if (e.key !== 'Enter')
       return
 
-    dispatch(applyWorkingViewportSize())
+    _applyWorkingViewportSize()
   }
 
-  const onBlur = () => {
-    dispatch(applyWorkingViewportSize())
-  }
+  const onBlur = () => _applyWorkingViewportSize()
 
   const onWidthChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     dispatch(updateWorkingViewportSize({
@@ -31,9 +70,22 @@ export const render = () => {
     }))
   }
 
+  useEffect(() => {
+    const timeoutId = setInterval(() => {
+      updateSizeRestrictions(
+        { width: isWidthRestricted, height: isHeightRestricted },
+        determineSizeRestrictions(el.current, size),
+        setIsWidthRestricted,
+        setIsHeightRestricted,
+      )
+    }, 1000)
+    return () => clearTimeout(timeoutId)
+  }, [el, size, isWidthRestricted, isHeightRestricted])
+
   return (
     <div className="viewport-info-bar">
       <input
+        className={isWidthRestricted ? 'invalid' : ''}
         type="number"
         onChange={onWidthChange}
         onBlur={onBlur}
@@ -42,6 +94,7 @@ export const render = () => {
       />
       <div className="dimensions-seperator"><i className="fas fa-xmark" /></div>
       <input
+        className={isHeightRestricted ? 'invalid' : ''}
         type="number"
         onChange={onHeightChange}
         onBlur={onBlur}
