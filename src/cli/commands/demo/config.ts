f56e35b3a278019b811944ecb690620dddf36d1e
samhuk <samhughes1701@hotmail.co.uk>
2023-03-07 23:18:26 +0000

    Add 'outDir' to CLI args (as well as the existing config), for ease of toggling it quickly.

--- src/cli/commands/demo/config.ts
+++ src/cli/commands/demo/config.ts
@@ -9,8 +9,8 @@ const createError = (causedBy: ExhString): ExhError => createExhError({
   causedBy,
 })
 
-const createInvalidBooleanError = (argName: string, recievedValue: any) => (
-  createError(c => `argument '${c.bold(argName)}' is not a valid boolean. Received: ${c.cyan(JSON.stringify(recievedValue))}`)
+const createInvalidTypeError = (argName: string, recievedValue: any, expectedType: string) => (
+  createError(c => `argument '${c.bold(argName)}' is not a valid ${expectedType}. Received: ${c.cyan(JSON.stringify(recievedValue))}`)
 )
 
 export const applyDemoOptionsToConfig = (
@@ -19,9 +19,18 @@ export const applyDemoOptionsToConfig = (
 ): ExhError | null => {
   if (options.verbose != null) {
     if (typeof options.verbose !== 'boolean')
-      return createInvalidBooleanError('verbose', options.verbose)
+      return createInvalidTypeError('verbose', options.verbose, 'boolean')
 
     config.verbose = options.verbose
+  }
+
+  if (options.outDir != null) {
+    if (typeof options.outDir !== 'string')
+      return createInvalidTypeError('outDir', options.outDir, 'string')
+    if (options.outDir.length === 0)
+      return createError(c => `argument '${c.bold('outDir')}' cannot be empty. Received: ${c.cyan(JSON.stringify(options.outDir))}`)
+
+    config.demo.outDir = options.outDir
   }
 
   return null
--- src/cli/commands/demo/types.ts
+++ src/cli/commands/demo/types.ts
@@ -2,4 +2,5 @@ import { BaseCliArgumentsOptions } from '../../types'
 
 export type DemoCliArgumentsOptions = BaseCliArgumentsOptions & {
   verbose?: boolean
+  outDir?: string
 }
