3945d539b93b3a64480d8e6b93ecd49c559787bd
samhuk <samhughes1701@hotmail.co.uk>
2023-05-14 15:13:01 +0100

    Improve how the css theme is applied on initial page load. v2.3.4.

--- src/site/client/connectors/theme.ts
+++ src/site/client/connectors/theme.ts
@@ -1,7 +1,9 @@
 import { getCookieValue, setCookieValue } from './cookie'
 
+import { THEMES } from '../../../common/theme'
+
 const COOKIE_NAME = 'theme'
 
-export const getTheme = () => getCookieValue(COOKIE_NAME)
+export const getTheme = () => getCookieValue(COOKIE_NAME) ?? THEMES[0]
 
 export const saveTheme = (theme: string) => setCookieValue(COOKIE_NAME, theme)
